c158de64788da2dbb701ad594966dbde4e19bda9
Kevin Kozlowski <Kevin.Kozlowski@gmail.com>
2018-03-11 10:18:40 -0600

    Removed default secret key

--- config.py
+++ config.py
@@ -2,7 +2,11 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-  SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-key-is-super-secret'
+  def __init__(self):
+    if 'SECRET_KEY' in os.environ:
+      self.SECRET_KEY = os.environ.get('SECRET_KEY')
+    else:
+      raise Exception("Must set environment variable SECRET_KEY")
   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     'sqlite:///' + os.path.join(basedir, 'app.db')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
