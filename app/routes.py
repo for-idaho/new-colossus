7eae51019520929e92550750c4a086c11e2efe48
Kevin Kozlowski <Kevin.Kozlowski@gmail.com>
2018-03-16 15:21:46 -0600

    First pass at JWT authentication

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,27 @@
+import sys
 from flask import render_template, flash, redirect, url_for, request, jsonify
-from flask_jwt import JWT, jwt_required, current_identity
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    jwt_refresh_token_required, create_refresh_token,
+    get_jwt_identity, set_access_cookies,
+    set_refresh_cookies, unset_jwt_cookies
+)
 from app import app, db
 from app.models import Candidate
+import uuid
 
 
-def authenticate(username, password):
-    user = User.query.filter_by(username=username)
+def authenticate(email, password):
+    user = Candidate.query.filter_by(email=email)
     if user is not None and user.check_password(password):
         return user
+
+
+def identity(payload):
+  print(payload)
+  user_id = payload['identity']
+  return Candidate.query.filter_by(user_id, None)
+
 
 @app.route('/')
 @app.route('/index')
@@ -17,9 +31,92 @@ def index():
 
 @app.route('/register', methods=['POST'])
 def register():
-    data = request.body
+    print("Posted to /register")
+    data = request.get_json()
+    #Check if email is already in use
+    check_candidate = Candidate.query.filter_by(email=data['email'])
+    if(check_candidate is not None):
+      return jsonify({'status': 'Failure', 'error': 'Email is already in use. Please use another'})
+    candidate = Candidate(candidate_id=uuid.uuid4().hex,
+                          username=data['username'],
+                          email=data['email'],
+                          first_name=data['first_name'],
+                          middle_name=data['middle_name'],
+                          last_name=data['last_name'])
+    candidate.set_password(data['password'])
+    try:
+      db.session.add(candidate)
+      db.session.commit()
+      return jsonify({'status': 'Success'})
+    except:
+      print('Unexpected error:', sys.exc_info()[0])
+      return jsonify({'status': 'Failure', 'error': 'Unknown error - please try again later'})
 
-@app.route('/test_protected'):
+
+jwt = JWTManager(app)
+
+
+# By default, the CRSF cookies will be called csrf_access_token and
+# csrf_refresh_token, and in protected endpoints we will look for the
+# CSRF token in the 'X-CSRF-TOKEN' header. You can modify all of these
+# with various app.config options. Check the options page for details.
+
+
+# With JWT_COOKIE_CSRF_PROTECT set to True, set_access_cookies() and
+# set_refresh_cookies() will now also set the non-httponly CSRF cookies
+# as well
+@app.route('/token/auth', methods=['POST'])
+def login():
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+
+    Candidate.query.filter_by(email=email)
+
+    check_candidate = Candidate.query.filter_by(email=email).first()
+    if check_candidate is None or not check_candidate.check_password(password):
+        return jsonify({'login': False}), 401
+
+    # Create the tokens we will be sending back to the user
+    access_token = create_access_token(identity=email)
+    refresh_token = create_refresh_token(identity=email)
+
+    # Set the JWTs and the CSRF double submit protection cookies
+    # in this response
+    resp = jsonify({'login': True})
+    set_access_cookies(resp, access_token)
+    set_refresh_cookies(resp, refresh_token)
+    return resp, 200
+
+
+@app.route('/token/refresh', methods=['POST'])
+@jwt_refresh_token_required
+def refresh():
+    # Create the new access token
+    current_user = get_jwt_identity()
+    access_token = create_access_token(identity=current_user)
+
+    # Set the access JWT and CSRF double submit protection cookies
+    # in this response
+    resp = jsonify({'refresh': True})
+    set_access_cookies(resp, access_token)
+    return resp, 200
+
+
+# Because the JWTs are stored in an httponly cookie now, we cannot
+# log the user out by simply deleting the cookie in the frontend.
+# We need the backend to send us a response to delete the cookies
+# in order to logout. unset_jwt_cookies is a helper function to
+# do just that.
+@app.route('/token/remove', methods=['POST'])
 @jwt_required
-def test_protected():
-    return '{}'.format(current_identity)
+def logout():
+    resp = jsonify({'logout': True})
+    unset_jwt_cookies(resp)
+    return resp, 200
+
+
+@app.route('/api/example', methods=['GET'])
+@jwt_required
+def protected():
+    email = get_jwt_identity()
+    return jsonify({'hello': 'from {}'.format(email)}), 200
--- config.py
+++ config.py
@@ -10,3 +10,23 @@ class Config(object):
   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     'sqlite:///' + os.path.join(basedir, 'app.db')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
+  # Configure application to store JWTs in cookies
+  JWT_TOKEN_LOCATION = ['cookies']
+
+  # Only allow JWT cookies to be sent over https. In production, this
+  # should likely be True
+  JWT_COOKIE_SECURE = False if os.environ.get('STAGE') == 'DEV' else True
+
+  # Set the cookie paths, so that you are only sending your access token
+  # cookie to the access endpoints, and only sending your refresh token
+  # to the refresh endpoint. Technically this is optional, but it is in
+  # your best interest to not send additional cookies in the request if
+  # they aren't needed.
+  JWT_ACCESS_COOKIE_PATH = '/api/'
+  JWT_REFRESH_COOKIE_PATH = '/token/refresh'
+
+  # Enable csrf double submit protection. See this for a thorough
+  # explanation: http://www.redotheweb.com/2015/11/09/api-security.html
+  JWT_COOKIE_CSRF_PROTECT = True
+
+  JWT_SECRET_KEY = os.environ.get('SECRET_KEY')
