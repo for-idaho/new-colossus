110f09f35ad13892f151ae98bfd930f360510127
Flaqueeau <econrad@zagmail.gonzaga.edu>
2018-03-16 11:44:08 -0600

    remove src

--- src/template/test_template.py
+++
@@ -1,7 +0,0 @@
-from template import getTemplate
-import os 
-
-def test_template():
-    """ We create a template from a jinja file without error """
-    path = os.path.join(os.path.dirname(__file__), "assets/foo.html")
-    assert getTemplate(path) != None
