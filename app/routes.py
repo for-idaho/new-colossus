e11ba811e2fa16fadb19c064689932fb5908c176
Flaqueeau <econrad@zagmail.gonzaga.edu>
2018-03-16 18:26:29 -0600

    Connect POST /sites/new to s3 deploy

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from flask import render_template, flash, redirect, url_for, request, jsonify
 from app import app, db
-from deploy.s3 import put
+from deploy.s3 import create_bucket
+import json
+from jsonschema import validate
+import time
 
 
 @app.route('/')
@@ -11,4 +14,21 @@ def index():
 
 @app.route('/sites/new', methods=["POST"])
 def new_site():
-    return "TODO"
+    schema = {
+        "files": {
+            "index": {"type": "string"},
+            "error": {"type": "string"}
+        }
+    }
+
+    data = request.get_json()
+    validate(data, schema)
+
+    files = data['files']
+
+    # TODO: Change this to be the JWT user when auth is working
+    # timestamp right now for easy-ish testing
+    name_TODO_CHANGE_ME_TO_JWT_USER = str(time.time())
+
+    return create_bucket(name_TODO_CHANGE_ME_TO_JWT_USER,
+                         files['index'], files['error'])
